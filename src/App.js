af1c274eb8c904c5c9299261b144b36ed1398ea4
Bak David <bak.david96@gmail.com>
2021-10-30 13:09:03 +0300

    Finalized project with local data save, todo list clear and checkbox

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React, {useState, useRef} from "react";
+import React, {useState, useRef, useEffect} from "react";
 import TodoList from "./TodoList";
 
 function App() {
   const [todos, setTodos] = useState([]);
   const todoNameRef = useRef()
+
+  const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if(storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id){
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
 
   // this is to generate unique keys for elements
   const generateKey = (pre) => {
@@ -15,7 +33,6 @@ function App() {
     return `${name}_${new Date().getTime()}`;
   }
 
-
   function handleAddTodo(e){
     const name = todoNameRef.current.value
     if(name === '') return
@@ -24,13 +41,19 @@ function App() {
     })
     todoNameRef.current.value = null
   }
+
+  function handleClearTodo(){
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
   return (
     <>
-      <TodoList todos={todos}/>
+      <TodoList todos={todos} toggleTodo={toggleTodo}/>
       <input ref={todoNameRef} type="text"/>
       <button onClick={handleAddTodo}>Add Todo</button>
-      <button>Clear Completed Todos</button>
-      <div>0 left to do</div>
+      <button onClick={handleClearTodo}>Clear Completed Todos</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
     </>
   )
 }
