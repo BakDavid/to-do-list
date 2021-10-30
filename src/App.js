df7db6d721c627c765fb18707910cc8fcf5acf1b
Bak David <bak.david96@gmail.com>
2021-10-30 12:26:01 +0300

    Adding ability to add new todos to our list and making unique keys

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import TodoList from "./TodoList";
 
 function App() {
-  const [todos, setTodos] = useState(['Todo 1','Todo 2']);
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef()
+
+  // this is to generate unique keys for elements
+  const generateKey = (pre) => {
+    return `${ pre }_${ new Date().getTime() }`;
+  }
+
+  // this is my function that I rewrote, it's similar to the previus one
+  function myGenerateKey(name){
+    return `${name}_${new Date().getTime()}`;
+  }
+
+
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value
+    if(name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: myGenerateKey(name), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
   return (
     <>
       <TodoList todos={todos}/>
-      <input type="text"/>
-      <button>Add Todo</button>
+      <input ref={todoNameRef} type="text"/>
+      <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear Completed Todos</button>
       <div>0 left to do</div>
     </>
